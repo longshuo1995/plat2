8697af8c31e16989101b89b69eece0504451d71c
longshuo <longshuo@zuoyebang.com>
2019-03-09 10:36:55 +0800

    t

--- www.py
+++ www.py
@@ -3,7 +3,7 @@ from web.controllers.account.Account import route_account
 from web.controllers.index import route_index
 from web.controllers.static import route_static
 from web.controllers.user.User import route_user
-from web.controllers.api import route_api, Food, Member
+from web.controllers.api import route_api, Food, Member, goods
 
 
 app.register_blueprint(route_index, url_prefix='/')
