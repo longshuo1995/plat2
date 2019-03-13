a4761d115cb1262a48ad46e8632376e96e90ce56
longshuo <longshuo@zuoyebang.com>
2019-03-13 21:21:36 +0800

    t

--- use_test/07_test_openssl.py
+++ use_test/07_test_openssl.py
@@ -14,4 +14,4 @@ def index():
 if __name__ == "__main__":
     # attention2:你的以及域名，比如bbaidu.com
     # app.config['SERVER_NAME'] = 'aishangnet.club'
-    app.run('0.0.0.0', debug=True, port=80, ssl_context=('1_aishangnet.club_bundle.crt', '2_aishangnet.club.key'))
+    app.run('0.0.0.0', debug=True, port=80)
