680bdc585200f3e6a1be7f46712d7285e4c404e1
longshuo <longshuo@zuoyebang.com>
2019-03-14 20:03:01 +0800

    t

--- use_test/07_test_openssl.py
+++ use_test/07_test_openssl.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 
 
 # attention1: 原来是app.route('/')
-@app.route('/')
+@app.route('/test')
 def index():
-    return u'这个是api的首页'
+    return 'test....'
 
 
 if __name__ == "__main__":
-    # attention2:你的以及域名，比如bbaidu.com
     app.run('0.0.0.0', debug=True, port=8885)
