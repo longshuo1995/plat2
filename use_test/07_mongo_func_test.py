4d108a6b3372b37206815a3ea8f295e52b093c0d
longshuo <longshuo@zuoyebang.com>
2019-03-17 17:30:27 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -1,12 +1,12 @@
 from common.libs import db_mongo
 
 
-def getNextValue(user_Name):
-    ret = db_mongo.get_table('plat2', 'counters').find_and_modify({"_id": user_Name}, {"$inc": {"req": 1}}, safe=True)
+def getNextValue(key_name):
+    ret = db_mongo.get_table('plat2', 'counters').find_and_modify({"_id": key_name}, {"$inc": {"req": 1}}, safe=True, new=True)
     new = ret["req"]
     return new
 
 
-print(getNextValue("goodid"))
+print(getNextValue("userid"))
 
 
