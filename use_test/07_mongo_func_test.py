a6984d566e8e43b3b2aeeb1631ea63454f74966a
longshuo <longshuo@zuoyebang.com>
2019-03-17 17:23:13 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -2,9 +2,9 @@ from common.libs import db_mongo
 
 
 def getNextValue(user_Name):
-    ret = db_mongo.get_table('plat2', 'counter').update({"_id": user_Name}, {"$inc": {"req": 1}})
-    # new = ret["sequence_value"]
-    return ret
+    ret = db_mongo.get_table('plat2', 'counters').find_and_modify({"_id": user_Name}, {"$inc": {"req": 1}}, safe=True)
+    new = ret["sequence_value"]
+    return new
 
 
 print(getNextValue("userid"))
