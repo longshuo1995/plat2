8cee15789d58ff3e4613f4bac3a690f6578ad87f
longshuo <longshuo@zuoyebang.com>
2019-03-17 22:18:24 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -1,12 +1,5 @@
 from common.libs import db_mongo
 
 
-def getNextValue(key_name):
-    ret = db_mongo.get_table('plat2', 'counters').find_and_modify({"_id": key_name}, {"$inc": {"req": 1}}, safe=True, new=True)
-    new = ret["req"]
-    return new
-
-
-print(getNextValue("userid"))
-
-
+ret = db_mongo.get_table('plat2', 'good').find({"name": {"$regex": ".*童装.*"}})
+print(list(ret))
--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -1,4 +1,6 @@
 from flask import jsonify, request
+
+from common.libs import db_mongo
 from web.controllers.api import route_api
 
 test_url = 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1976195564,3037788353&fm=27&gp=0.jpg'
@@ -9,15 +11,13 @@ def foodIndex():
     resp = {'code': 200, 'msg': '操作成功', 'data': {}}
     # 所有类
     data_cat_list = [{'id': 0, 'name': '全部'}]
-    # TODO 需要修改成查询数据库
-    cat_list = [(1, '免单商品'), (2, '高佣商品'), (3, '排行榜')]
-    if cat_list:
-        for item in cat_list:
-            data_cat_list.append({'id': item[0], 'name': item[1]})
+    items = db_mongo.get_table('plat2', 'good_tp').find()
+    for item in items:
+        data_cat_list.append({'id': item['_id'], 'name': item['name']})
     resp['data']['cat_list'] = data_cat_list
 
     # food list
-    food_list = [(1, '免单商品'), (2, '高佣商品'), (3, '排行榜')]
+    food_list = [(1, 'test'), (2, 'test'), (3, 'test')]
     data_food_list = []
     if food_list:
         for item in food_list:
@@ -35,7 +35,12 @@ def foodSearch():
     resp = {'code': 200, 'msg': '操作成功', 'data': {}}
     req = request.values
     cat_id = req.get('cat_id', 0)
+    query = {}
     mix_kw = req.get('mix_kw', '')
+    if cat_id:
+        query['_id'] = int(cat_id)
+    if mix_kw:
+        query['name'] = {"name": ""}
     p = int(req.get('p', 1))
     offset = (p-1) * page_size
 
