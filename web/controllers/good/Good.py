127bf850ac8a4967a27f370095c35d3e75943c7e
longshuo <longshuo@zuoyebang.com>
2019-03-17 11:15:10 +0800

    t

--- web/controllers/good/Good.py
+++ web/controllers/good/Good.py
@@ -14,7 +14,8 @@ def good_list():
     good_list = list(db_mongo.get_table('plat2', 'good_list').find().skip(skip_count).limit(PAGER_PER_COUNT))
     data = {
         "good_tp_list": good_tp_list,
-        "good_list": good_list
+        "good_list": good_list,
+        "test": 'longshuo'
     }
     return render_template('good/good_list.html', data=data)
 
