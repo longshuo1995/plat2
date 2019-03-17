1721460fcf44f97f6bd645aeb384fbe3775566e6
longshuo <longshuo@zuoyebang.com>
2019-03-17 17:07:44 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -1,10 +1,9 @@
 from common.libs import db_mongo
 
 r = db_mongo.get_table('plat2', 'counter').find_and_modify(
-    {
-        "query": {"_id": "userid"},
-        "update": {"$inc": {"seq": 1}}
-    }
+        {"_id": "userid"},
+        {"$inc": {"seq": 1}},
+        safe=True, new=True
 )
 print(r)
 
