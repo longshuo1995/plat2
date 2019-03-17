df70aaa505e25f9a97b778bd3eb7ca9dd350b380
longshuo <longshuo@zuoyebang.com>
2019-03-17 17:09:21 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -1,9 +1,10 @@
 from common.libs import db_mongo
 
-r = db_mongo.get_table('plat2', 'counter').find_and_modify(
+tb = db_mongo.get_table('plat2', 'counter')
+new = tb.find_and_modify(
         {"_id": "userid"},
         {"$inc": {"seq": 1}},
         safe=True, new=True
 )
-print(r)
+print(new['seq'])
 
