b524e85464b74cb62c435d2a470ed8aa55f95aad
longshuo <longshuo@zuoyebang.com>
2019-03-17 17:13:25 +0800

    t

--- use_test/07_mongo_func_test.py
+++ use_test/07_mongo_func_test.py
@@ -1,10 +1,7 @@
 from common.libs import db_mongo
 
-tb = db_mongo.get_table('plat2', 'counter')
-new = tb.find_and_modify(
-        {"_id": "userid"},
-        {"$inc": {"seq": 1}},
-        safe=True, new=True
-)
-print(new)
+db_mongo.get_table('plat2', 'test').insert_one({
+    "_id": {"$where": 'getNextSequence("userid")'}
+})
+
 
