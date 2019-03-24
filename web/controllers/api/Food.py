902797f42c6d9bfc887064afd3c4ddb2fa14e8ec
longshuo <longshuo@zuoyebang.com>
2019-03-25 05:10:07 +0800

    t

--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -44,11 +44,11 @@ def foodSearch():
 
     mix_kw = req.get('mix_kw', '').split()
     keyword = ' '.join(mix_kw)
-
+    print("*" * 11)
+    print(cat_id)
     resp_jo = pdd_tools.search_goods(keyword=keyword, sort_type=cat_id, p=p)
     data_food_list = []
     for item in resp_jo.get('goods_search_response', {}).get('goods_list', []):
-        print(item)
         promotion_rate = item.get('promotion_rate')
         promotion_rate = promotion_rate if promotion_rate else 0
         quan_price = item.get('mall_coupon_min_order_amount', 0)
