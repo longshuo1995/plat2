f7890b4552c6cc6b1717815fc8048bfcb60e7b3a
longshuo <longshuo@zuoyebang.com>
2019-03-25 05:11:36 +0800

    t

--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -38,6 +38,7 @@ def foodSearch():
 
     cat_id = int(req.get('cat_id', 0))
     cat_id = cat_id if cat_id else 1
+    cat_id = 0 if cat_id == 1 else cat_id
 
     p = int(req.get('p', 1))
     p = p if p else 1
