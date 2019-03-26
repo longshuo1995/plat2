88e7a7a85c26a45101eafd14769aa062704de67c
longshuo <longshuo@zuoyebang.com>
2019-03-26 22:20:07 +0800

    t

--- web/controllers/api/Order.py
+++ web/controllers/api/Order.py
@@ -1,6 +1,8 @@
 import json
 
 from flask import request, jsonify
+
+from common.libs import db_mongo
 from web.controllers.api import route_api
 
 
@@ -9,16 +11,32 @@ def my_order():
     resp = {'code': 200, 'msg': '操作成功~', 'data': {}}
     req = request.values
     status = int(req['status']) if 'status' in req else 0
-    pay_order_list = [
-        {
-            'status': 0,
-            'title': '袜子 10双 20双  袜女士防钩丝袜。。。。',
-            'date': '2019-03-09',
-            'status_desc': 'status_desc',
-            'order_number': 'order_number',
-            'note': 'note'
+    open_id = req.get('openid')
+
+    infos = db_mongo.get_table('plat2', 'order').find({'custom_parameters': open_id})
+    order_list = []
+    for info in infos:
+        member_info = db_mongo.get_table('plat2', 'member').find_one({'_id': info['custom_parameters']})
+        rate = 0
+        if info['custom_parameters'] == open_id:
+            rate += 0.2
+        if info.get('refer_id') == open_id:
+            rate += 0.2
+        if info.get('leader_openid') == open_id:
+            rate += 0.6
+        if info.get('leader_master') == open_id:
+            rate += 0.1
+        temp = {
+            'own_icon': member_info.get('icon_url'),
+            'own_name': member_info.get('nick_name', ''),
+            'order_status_desc': info.get('order_status_desc', ''),
+            'goods_name': info.get('goods_name', ''),
+            'goods_thumbnail_url': info.get('goods_thumbnail_url', ''),
+            'pay_money': info.get('order_amount')/100,
+            'self_money': info.get('order_amount')*info.get('promotion_rate')*rate/100000,
         }
-    ]
-    resp['data']['pay_order_list'] = pay_order_list
+        order_list.append(temp)
+
+    resp['data']['order_list'] = order_list
     return jsonify(resp)
 
