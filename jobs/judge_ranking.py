39fabc7ebba90e15adaa9df16e53bd03a38db666
longshuo <longshuo@zuoyebang.com>
2019-03-26 11:28:30 +0800

    t

--- jobs/judge_ranking.py
+++ jobs/judge_ranking.py
@@ -11,7 +11,9 @@ DAY_SECONDS = 24*60*60
 def judge_24h():
     c_time = int(time.time())
     door_time = c_time - 30 * DAY_SECONDS
-    items = db_mongo.get_table('plat2', 'order').find({'order_create_time': {'$gt': door_time}})
+    items = db_mongo.get_table('plat2', 'order').find(
+        {'order_create_time': {'$gt': door_time}},
+        {'goods_id': 1})
     for item in items:
         print(item)
 
