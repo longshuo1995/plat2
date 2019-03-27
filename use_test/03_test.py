16ebf4c49c7e4fe40b1addd02212c03d675bf8be
longshuo <longshuo@zuoyebang.com>
2019-03-27 16:36:36 +0800

    t

--- use_test/03_test.py
+++ use_test/03_test.py
@@ -2,5 +2,6 @@ from common.libs import db_mongo
 
 infos = db_mongo.get_table('plat2', 'member').find_one({
     '$or': [{'refer_id': 'test'}, {'leader_openid': 'test'}],
+    'level': 0
 })
 print(infos)
