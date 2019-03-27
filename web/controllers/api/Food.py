b3cb66fed9842d6fc9f2b34dc2bae3890dbfd663
longshuo <longshuo@zuoyebang.com>
2019-03-27 22:05:48 +0800

    t

--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -17,15 +17,15 @@ def foodIndex():
         data_cat_list.append({'id': item['_id'], 'name': item['name']})
     resp['data']['cat_list'] = data_cat_list
 
-    # food list
-    food_list = [(1, 'test'), (2, 'test'), (3, 'test')]
     data_food_list = []
-    if food_list:
-        for item in food_list:
-            data_food_list.append({
-                'id': item[0],
-                'pic_url': test_url
-            })
+
+    resp_jo = pdd_tools.search_goods(keyword='', sort_type=0, p=1)
+    search_list = resp_jo.get('goods_search_response', {}).get('goods_list', [])
+    for item in search_list[:3]:
+        data_food_list.append({
+            'id': item['goods_id'],
+            'pic_url': item['goods_thumbnail_url'],
+        })
     resp['data']['banner_list'] = data_food_list
     return jsonify(resp)
 
@@ -39,15 +39,15 @@ def foodSearch():
     cat_id = int(req.get('cat_id', 0))
     cat_id = cat_id if cat_id else 1
     # cat_id = 0 if cat_id == 1 else cat_id
-
     p = int(req.get('p', 1))
     p = p if p else 1
 
     mix_kw = req.get('mix_kw', '').split()
     keyword = ' '.join(mix_kw)
     resp_jo = pdd_tools.search_goods(keyword=keyword, sort_type=cat_id, p=p)
+    search_list = resp_jo.get('goods_search_response', {}).get('goods_list', [])
     data_food_list = []
-    for item in resp_jo.get('goods_search_response', {}).get('goods_list', []):
+    for item in search_list:
         promotion_rate = item.get('promotion_rate')
         promotion_rate = promotion_rate if promotion_rate else 0
         quan_price = item.get('coupon_discount', 0)/100
