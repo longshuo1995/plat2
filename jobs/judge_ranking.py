622f3ce5ebe28fcea0be4531286f42b08bc81bd6
longshuo <longshuo@zuoyebang.com>
2019-03-28 18:00:12 +0800

    t

--- jobs/judge_ranking.py
+++ jobs/judge_ranking.py
@@ -35,7 +35,7 @@ def judge_local(offset_time, file_nm):
     path_nm = os.path.join(project_conf.project_path, 'asserts', file_nm)
     out_file = open(path_nm, 'w')
     for good_id in value_count.index:
-        info = db_mongo.get_table('plat2', 'order').find_one({'goods_id': good_id, '$ne': {'duo_coupon_amount': 4}})
+        info = db_mongo.get_table('plat2', 'order').find_one({'goods_id': good_id, 'duo_coupon_amount': {'$ne': 4}})
         # if not info.get('duo_coupon_amount'):
         #     continue
         temp = {
@@ -141,7 +141,7 @@ def calc_top_promotion(offset_time):
 
 
 if __name__ == '__main__':
-    calc_top_promotion(1100000)
-    calc_top_user(1100000)
+    # calc_top_promotion(1100000)
+    # calc_top_user(1100000)
     judge_24h()
 
