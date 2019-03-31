aa12af9a4e3603b438326f31a78faaff67149214
longshuo <longshuo@zuoyebang.com>
2019-03-31 09:20:17 +0800

    t

--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -48,9 +48,7 @@ def foodSearch():
     search_list = resp_jo.get('goods_search_response', {}).get('goods_list', [])
     data_food_list = []
     for item in search_list:
-        print(item)
-        print(item['promotion_rate'])
-        promotion_rate = item.get('promotion_rate')
+        promotion_rate = item.get('promotion_rate')/1000
         promotion_rate = promotion_rate if promotion_rate else 0
         quan_price = item.get('coupon_discount', 0)/100
         quan_price = quan_price if quan_price else 0
@@ -59,8 +57,6 @@ def foodSearch():
         min_price = row_price-quan_price
         min_price = round(min_price, 2)
 
-        if promotion_rate > 100:
-            promotion_rate /= 100
 
         promotion = int(promotion_rate)*min_price / 100
         promotion = round(promotion, 2)
