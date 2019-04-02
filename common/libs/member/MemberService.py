fb3c7f4cf27cc1380ccbbdfa40ca3a36daa21366
longshuo <longshuo@zuoyebang.com>
2019-04-02 21:24:24 +0800

    t

--- common/libs/member/MemberService.py
+++ common/libs/member/MemberService.py
@@ -1,17 +1,18 @@
 import requests
 
-from application import app
+# from application import app
+from config import base_setting
 
 
 def getWechatOpenId(code):
     url = 'https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={js_code}&grant_type' \
-          '=authorization_code'.format(appid=app.config['MINA_APP']['appid'], js_code=code, secret=app.config['MINA_APP']['appkey'])
+          '=authorization_code'.format(appid=base_setting.MINA_APP['appid'], js_code=code, secret=base_setting.MINA_APP['appkey'])
     jo = requests.get(url).json()
     return jo.get('openid', '')
 
 
 def get_access_token():
     url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % \
-              (app.config['MINA_APP']['appid'], app.config['MINA_APP']['appkey'])
+              (base_setting.MINA_APP['appid'], base_setting.MINA_APP['appkey'])
     jo = requests.get(url).json()
     return jo['access_token']
--- use_test/03_test.py
+++ use_test/03_test.py
@@ -1,8 +1,27 @@
-import os
+import requests
 
-import project_conf
+from common.libs.member import MemberService
 
-fn = os.path.join(project_conf.project_path, 'web', 'static', 'index_img')
-l = os.listdir(fn)
-print(l)
+
+def good_share(open_id):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    access_token = MemberService.get_access_token()
+    print(access_token)
+    path = 'pages/index/index?from_openid=%s' % open_id
+    url = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=%s" % access_token
+    data = {
+        'scene': 'from_openid=1'
+    }
+    content = requests.post(url, headers=headers, json=data).content
+    with open('艾艾.jpg', 'wb') as f:
+        print(content)
+        f.write(content)
+
+
+open_id = 'ohl4g5aJa1RVoZzZl7lAohgKXNT8'
+good_share(open_id)
+
+
 
