4d392e7349b358490688ffc6f8b4d0fe1cdf02f1
longshuo <longshuo@zuoyebang.com>
2019-04-04 09:06:28 +0800

    t

--- web/controllers/api/Food.py
+++ web/controllers/api/Food.py
@@ -78,17 +78,20 @@ def foodSearch():
 
 @route_api.route("/good/get_pdd_url")
 def get_pdd_url():
+    '''
+    {'avg_desc': 450, 'mall_coupon_remain_quantity': None, 'category_name': '海淘', 'coupon_remain_quantity': 13000, 'avg_serv': 461, 'avg_lgst': 463, 'serv_pct': 0.0609, 'promotion_rate': 200, 'sold_quantity': 24371, 'cat_ids': [16989, 17007, 17106, 0], 'coupon_min_order_amount': 300, 'lgst_pct': 0.0855, 'category_id': 12, 'mall_coupon_discount_pct': None, 'mall_id': 955517893, 'goods_eval_score': 4.51, 'cat_id': None, 'mall_name': '信德兴家居生活专营店', 'coupon_total_quantity': 20000, 'desc_pct': 0.0482, 'mall_coupon_min_order_amount': None, 'mall_coupon_end_time': None, 'merchant_type': 5, 'goods_name': '【买1送1】漏水拖鞋女夏居家沐浴软底防滑男家用情侣网红凉拖鞋', 'goods_eval_count': 1408, 'goods_id': 6869428690, 'goods_gallery_urls': ['https://t00img.yangkeduo.com/goods/images/2019-04-01/d0f9b879-2b0c-4347-9a9e-0cdade1be444.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-01/2d030fce-9eec-46d7-bef6-f9d3d7219068.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-01/a615e3d7-0db6-49f4-a177-8364d97878f9.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-01/fc5ff487-860b-4dd3-89d8-ddd6b19d2240.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-01/93b37d40-5684-4d6a-bbd6-9ad2d1845c55.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-03-27/afc35a91-55f5-432a-abbc-c3ed14633814.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-01/d9cc6a57-c214-40ec-852e-5cdc2bcb55f7.jpg', 'https://t00img.yangkeduo.com/goods/images/2019-04-02/62010dc4-6e42-413a-b0f8-f6386ef8221d.jpg'], 'mall_coupon_id': None, 'goods_desc': '【码数偏小一码 ,下单请拍大一码】、【码数偏小一码 ,下单请拍大一码】、【码数偏小一码 ,下单请拍大一码】重要的事情说三遍!!! 比如平时穿38码,下单请拍38/39;平时穿40码,下单请拍41/42,如此类推。', 'opt_name': '海淘', 'goods_thumbnail_url': 'https://t00img.yangkeduo.com/goods/images/2019-04-01/d94a5bb66079607587f740d57fa96e49.jpeg', 'opt_id': 12, 'opt_ids': [387, 419, 294, 329, 12, 223, 15], 'goods_image_url': 'https://t00img.yangkeduo.com/goods/images/2019-03-25/7a29527f-be99-4b40-8042-7117d7e6eeac.jpg', 'min_normal_price': 1090, 'has_coupon': True, 'has_mall_coupon': False, 'mall_coupon_start_time': None, 'mall_rate': 10, 'mall_coupon_total_quantity': None, 'create_at': 1553491578, 'mall_coupon_max_discount_amount': None, 'min_group_price': 990, 'mall_cps': 1, 'coupon_start_time': 1554220800, 'coupon_discount': 300, 'coupon_end_time': 1554479999}
+    '''
     resp = {'code': 200, 'msg': '操作成功', 'data': ''}
     req = request.values
     good_id = req.get('good_id')
     open_id = req.get('open_id')
     res = pdd_tools.search_good_detail(good_id, open_id)
+    print(res)
     dt = res.get('goods_promotion_url_generate_response', {}).get('goods_promotion_url_list', [{}])[0]
     if dt:
         good_detail = dt.get('goods_detail', {})
         row_price = good_detail.get('min_group_price', 0)/100
         discount = good_detail.get('coupon_discount', 0)/100
-        print(good_detail)
         resp['data'] = {
             'promotion_rate': good_detail.get('promotion_rate', 0)/1000,
             'pics': good_detail.get('goods_gallery_urls', []),
