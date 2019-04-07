7b8649389ab02d172ef1a1d8e5a00606e565eb5c
longshuo <longshuo@zuoyebang.com>
2019-04-08 04:07:25 +0800

    t

--- spider/pdd_spider.py
+++ spider/pdd_spider.py
@@ -6,15 +6,18 @@ pattern_num = re.compile('\((\d*)\)')
 
 def get_mall_info(good_id):
     url = 'https://mobile.yangkeduo.com/goods.html?goods_id=%s' % good_id
-    print(url)
     html = requests.get(url, verify=False).text
-    print(html)
     xhtml = etree.HTML(html)
     mall_name = xhtml.xpath("//div[@class='goods-mall-name']//text()")
     mall_name = mall_name[0] if mall_name else ''
     mall_icon = xhtml.xpath("//img[@class='goods-mall-tag']/@src")
     mall_icon = mall_icon[0] if mall_icon else ''
-    desc = xhtml.xpath("//div[@class='dsr-center']//text()")
+    dsr_content = xhtml.xpath("//div[@class='dsr-center']//text()")
+    desc = []
+    for i in range(0, len(dsr_content), 2):
+        temp = (dsr_content[0], dsr_content[1])
+        desc.append(temp)
+
     review_num_str = ''.join(xhtml.xpath("//div[@class='goods-reviews-num']//text()"))
     search_res = pattern_num.search(review_num_str)
     review_num = int(search_res.group(1)) if search_res else 0
