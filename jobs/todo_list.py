8089afb399ec8284838f2cbcf6d774a7938a49a6
longshuo <longshuo@zuoyebang.com>
2019-04-14 22:36:12 +0800

    t

--- jobs/todo_list.py
+++ jobs/todo_list.py
@@ -1,3 +1,163 @@
 '''
-3.订单页面修改
+2.注册逻辑的检测
+3.一级类目过滤
+4.订单点击筛选
+5.奇遇榜切换到真实数据
+1.显示相关人员的订单
+{
+    "code": 200,
+    "data": [
+        {
+            "level": 1,
+            "opt_id": 1,
+            "opt_name": "食品",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 4,
+            "opt_name": "母婴",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 13,
+            "opt_name": "水果",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 14,
+            "opt_name": "女装",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 15,
+            "opt_name": "百货",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 16,
+            "opt_name": "美妆",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 18,
+            "opt_name": "电器",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 590,
+            "opt_name": "充值",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 743,
+            "opt_name": "男装",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 818,
+            "opt_name": "家纺",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 1281,
+            "opt_name": "鞋包",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 1282,
+            "opt_name": "内衣",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 1451,
+            "opt_name": "运动",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 1543,
+            "opt_name": "手机",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 1917,
+            "opt_name": "家装",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 2048,
+            "opt_name": "汽车",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 2478,
+            "opt_name": "电脑",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 2946,
+            "opt_name": "生活个护",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 2964,
+            "opt_name": "大家电",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 2974,
+            "opt_name": "家具",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 3162,
+            "opt_name": "数码",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 3175,
+            "opt_name": "文化",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 3279,
+            "opt_name": "健康",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 3526,
+            "opt_name": "办公",
+            "parent_opt_id": 0
+        },
+        {
+            "level": 1,
+            "opt_id": 5127,
+            "opt_name": "厨房电器",
+            "parent_opt_id": 0
+        },
+    ],
+    "msg": "操作成功"
+}
 '''
