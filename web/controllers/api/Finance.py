12cfb020833d3a6b503ad8a8a6010769434a4638
longshuo <longshuo@zuoyebang.com>
2019-04-21 04:46:57 +0800

    t

--- web/controllers/api/Finance.py
+++ web/controllers/api/Finance.py
@@ -1,5 +1,7 @@
 from flask import request, jsonify
+from pandas import DataFrame
 
+import project_conf
 from common.libs import db_mongo
 from web.controllers.api import route_api
 
@@ -15,11 +17,20 @@ def member_finance():
     # info =
     infos = list(db_mongo.get_table('plat2', 'order').find({'$or': [{'custom_parameters': open_id}, {'refer_id': open_id},
                 {'leader_openid': open_id}, {'leader_master': open_id}]}))
+    # 计算佣金
+    df = DataFrame(infos)
+    # 自己
+    p1 = df[df['custom_parameters'] == open_id]['total_promotion'].sum() * project_conf.rate_conf['refer_rate']
+    # 老师
+    p2 = df[df['refer_id'] == open_id]['total_promotion'].sum() * project_conf.rate_conf['refer_rate']
+    # 团长
+    p3 = df[df['refer_id'] == open_id]['total_promotion'].sum() * project_conf.rate_conf['refer_rate']
+    total_promotion = round(sum([p1, p2, p3]), 2)
     data = {
         "current_money": 0,
         "checking_money": 0,
         "order_num": len(infos),
-        "est_money": 0,
+        "est_money": total_promotion,
         "today_money": 0
     }
     resp['data'] = data
