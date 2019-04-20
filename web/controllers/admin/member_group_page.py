9c9d7c5702b7e54cd1e0e07891b6303f0cb04f31
longshuo <longshuo@zuoyebang.com>
2019-04-20 11:41:47 +0800

    t

--- web/controllers/admin/member_group_page.py
+++ web/controllers/admin/member_group_page.py
@@ -1,6 +1,9 @@
+import re
+
 from flask import request, render_template, redirect, make_response
 import logging
 
+from common.libs import db_mongo
 from web.controllers.admin import route_admin
 
 
@@ -29,11 +32,24 @@ def admin_list():
 
 @route_admin.route('/member')
 def admin_member():
+    user_count_per_page = 10
     username = request.cookies.get('username')
     password = request.cookies.get('password')
+    req = request.values
+    pages = int(req.get('pages', 0))
 
+    mix_kw = req.get('mix_kw', '').strip()
+    query = {}
+    if mix_kw:
+        query['nick_name'] = re.compile(mix_kw)
+
+    user_info = list(db_mongo.get_table('plat2', 'member').find(query, {'open_id': 1, 'nick_name': 1, '_id': 0})
+                     .skip(pages*user_count_per_page).limit(user_count_per_page))
     if username == 'qiyupingtuan' and password == 'd665e0369613cdcaddd4d268b3bcfb90':
-        return render_template('admin/member.html')
+        data = {
+            'user_info': user_info
+        }
+        return render_template('admin/member.html', data=data)
     return render_template('admin/login.html')
 
 
