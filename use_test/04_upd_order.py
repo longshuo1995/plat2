ba3d8e39fad7f9418e208d1eb7fa630ce2b2673e
longshuo <longshuo@zuoyebang.com>
2019-04-22 23:31:38 +0800

    t

--- use_test/04_upd_order.py
+++ use_test/04_upd_order.py
@@ -4,5 +4,8 @@ items = db_mongo.get_table('plat2', 'order').find()
 for item in items:
     open_id = item['custom_parameters']
     info = db_mongo.get_table('plat2', 'order').find_one({'_id': open_id})
+    if not info:
+        print(item)
+        continue
     db_mongo.get_table('plat2', 'order').update({'_id': item['_id']}, {'$set': {'refer_id': info['refer_id'], 'leader_openid': info['leader_openid']}})
 
