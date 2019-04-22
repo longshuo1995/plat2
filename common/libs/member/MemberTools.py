a99348a467eaa6dce8fa4658bfbd8549e3b7e579
longshuo <longshuo@zuoyebang.com>
2019-04-22 22:58:50 +0800

    t

--- common/libs/member/MemberTools.py
+++ common/libs/member/MemberTools.py
@@ -9,7 +9,7 @@ leader_master,
 
 
 def upgrade(open_id, set_leader_master=False):
-    tb = db_mongo.get_table('plat2', 'member')
+    tb = db_mongo.get_table('plat2', 'member_back')
     info = tb.find_one({'_id': open_id})
     leader_master = ''
     if set_leader_master:
@@ -26,13 +26,17 @@ def upgrade(open_id, set_leader_master=False):
 # 递归计算(member_id的所有下级...   都更新为leader_id的团员)
 def upgrade_leader(member_id, leader_id):
     # 所有子级
-    member_items = db_mongo.get_table('plat2', 'member').find({'refer_id': member_id, '_id': {'$ne': member_id}},
+    member_items = db_mongo.get_table('plat2', 'member_back').find({'refer_id': member_id, '_id': {'$ne': member_id}},
                                                               {'open_id': 1})
-    db_mongo.get_table('plat2', 'member').update_many({'refer_id': member_id}, {'$set': {'leader_openid': leader_id}})
+    db_mongo.get_table('plat2', 'member_back').update_many({'refer_id': member_id}, {'$set': {'leader_openid': leader_id}})
     for item in member_items:
         upgrade_leader(item['open_id'], leader_id)
 
 
+# def upgrade_order():
+
 if __name__ == '__main__':
-    # tb = db_mongo.get_table('plat2', 'member').find({'refer_openid': })
-    upgrade('ohl4g5USDznFdyo9qVFmZQcOn-6Q')
+    # items = db_mongo.get_table('plat2', 'member').find({'level': 1})
+    # for item in items:
+    #     upgrade(item['open_id'])
+    upgrade('ohl4g5cpaQ2bYaQe8esGz9xVbAxk')
