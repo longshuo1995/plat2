0c80e996907905926c89478a79d1e6259dfbe350
longshuo <longshuo@zuoyebang.com>
2019-05-02 14:49:05 +0800

    t

--- jobs/03_转换.py
+++ jobs/03_转换.py
@@ -1,6 +1,6 @@
 from common.libs import db_mongo
 
-items = list(db_mongo.get_table('plat2', 'member').find())
+items = list(db_mongo.get_table('plat2', 'member_id').find())
 for item in items:
     item['_id'] = db_mongo.getNextValue('member')
-    db_mongo.get_table('plat2', 'member_id').insert_one(item)
+    db_mongo.get_table('plat2', 'member').insert_one(item)
