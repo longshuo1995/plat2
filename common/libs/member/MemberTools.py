d6e2f517a8158cfffc3fa79fc3a7be76a77df42a
longshuo <longshuo@zuoyebang.com>
2019-05-03 10:21:51 +0800

    t

--- common/libs/member/MemberTools.py
+++ common/libs/member/MemberTools.py
@@ -17,7 +17,7 @@ def upgrade(open_id, set_leader_master=False):
         if leader_master == open_id:
             leader_master = ''
 
-    tb.update({'_id': open_id}, {'$set': {'level': 1, 'leader_openid': open_id,
+    tb.update({'open_id': open_id}, {'$set': {'level': 1, 'leader_openid': open_id,
                                           'refer_id': open_id, 'leader_master': leader_master}})
     # tb.update({'refer_id': open_id}, {'$set': {'leader_openid': open_id, 'leader_master': leader_master}})
     upgrade_leader(open_id, open_id)
@@ -26,7 +26,7 @@ def upgrade(open_id, set_leader_master=False):
 # 递归计算(member_id的所有下级...   都更新为leader_id的团员)
 def upgrade_leader(member_id, leader_id):
     # 所有子级
-    member_items = db_mongo.get_table('plat2', 'member').find({'refer_id': member_id, '_id': {'$ne': member_id}},
+    member_items = db_mongo.get_table('plat2', 'member').find({'refer_id': member_id, 'open_id': {'$ne': member_id}},
                                                               {'open_id': 1})
     db_mongo.get_table('plat2', 'member').update_many({'refer_id': member_id}, {'$set': {'leader_openid': leader_id}})
     for item in member_items:
--- use_test/05_calc_repeat.py
+++ use_test/05_calc_repeat.py
@@ -4,9 +4,10 @@ from common.libs import db_mongo
 
 items = db_mongo.get_table('plat2', 'member').find()
 df = pd.DataFrame(items)
-items = df['open_id'].groupby('open_id').sum()
+items = df['open_id'].sum()
 for item in items.index:
-    print(item)
-    print(items[item])
+    if int(items[item]) > 1:
+        print(item)
+        print(items[item])
 
 
