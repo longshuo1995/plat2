0938bef44470f7e4001a59b636ace641f97acad9
longshuo <longshuo@zuoyebang.com>
2019-05-03 11:42:47 +0800

    t

--- use_test/06_upd_order.py
+++ use_test/06_upd_order.py
@@ -4,5 +4,6 @@ items = db_mongo.get_table('plat2', 'order').find({'order_create_time': {'$gt': 
 for item in items:
     info = db_mongo.get_table('plat2', 'member').find_one({'open_id': item['custom_parameters']})
     if item['refer_id'] == '' or item['leader_openid'] == '':
-        db_mongo.get_table('plat2', 'order').update({'_id': item['_id']}, {'$set': {'refer_id': info['refer_id'], 'leader_openid': info['leader_openid']}})
+        # db_mongo.get_table('plat2', 'order').update({'_id': item['_id']}, {'$set': {'refer_id': info['refer_id'], 'leader_openid': info['leader_openid']}})
+        print(item)
 
