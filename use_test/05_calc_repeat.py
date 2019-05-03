4c4395c5986dcd797be94b44255a0e61b32a40e6
longshuo <longshuo@zuoyebang.com>
2019-05-03 09:34:13 +0800

    t

--- use_test/05_calc_repeat.py
+++ use_test/05_calc_repeat.py
@@ -5,7 +5,6 @@ from common.libs import db_mongo
 items = db_mongo.get_table('plat2', 'member').find()
 df = pd.DataFrame(items)
 items = df.groupby('open_id').sum()
-items = items.sort_values(ascending=False)
 for item in items.index:
     print(item)
     print(items[item])
