ed5d9b6888974d80c3b91b3abadb172ee3c459a6
longshuo <longshuo@zuoyebang.com>
2019-05-15 17:26:00 +0800

    t

---
+++ use_test/05_upd_id.py
@@ -0,0 +1,9 @@
+from common.libs import db_mongo
+import pandas as pd
+
+items = db_mongo.get_table('plat2', 'member').find()
+
+for item in items:
+    db_mongo.get_table('plat2', 'member').remove({'_id': item['_id']})
+    item['_id'] = int(item['_id'])
+    db_mongo.get_table('plat2', 'member').insert_one(item)
