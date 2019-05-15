9114cba7288e6e9c6e368dd3c50b14abda1145fb
longshuo <longshuo@zuoyebang.com>
2019-05-15 17:41:28 +0800

    t

--- use_test/05_upd_id.py
+++ use_test/05_upd_id.py
@@ -7,4 +7,4 @@ for item in items:
     db_mongo.get_table('plat2', 'member').remove({'_id': item['_id']})
     item['_id'] = int(item['_id'])
     db_mongo.get_table('plat2', 'member').insert_one(item)
-    
+
