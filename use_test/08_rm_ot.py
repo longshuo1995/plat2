2965be5480050f2ec755032fc21119220af510be
longshuo <longshuo@zuoyebang.com>
2019-05-22 21:06:03 +0800

    t

--- use_test/08_rm_ot.py
+++ use_test/08_rm_ot.py
@@ -3,4 +3,4 @@ from common.libs import db_mongo
 items = db_mongo.get_table('plat2', 'order').find()
 for item in items:
     if not isinstance(item['_id'], str):
-        print(item['_id'])
+        db_mongo.get_table('plat2', 'order').remove({'_id': item['_id']})
