bc4598d3e73d5de00e18c2121072f2308d55484a
longshuo <longshuo@zuoyebang.com>
2019-05-22 11:03:11 +0800

    t

--- jobs/update_ip_addr.py
+++ jobs/update_ip_addr.py
@@ -1,10 +1,10 @@
 from common.libs import db_mongo
 from common.libs.tools import IpSpider
 
-# items = db_mongo.get_table('plat2', 'member').find({'ip': {'$exists': True}, 'ip_addr': {'$exists': False}})
-# for item in items:
-#     ip_addr = IpSpider.start_crawl(item['ip'])
-#     db_mongo.get_table('plat2', 'member').update({'_id': item['_id']}, {'$set': {'ip_addr': ip_addr}})
+items = db_mongo.get_table('plat2', 'member').find({'ip': {'$exists': True}, 'ip_addr': {'$exists': False}})
+for item in items:
+    ip_addr = IpSpider.start_crawl(item['ip'])
+    db_mongo.get_table('plat2', 'member').update({'_id': item['_id']}, {'$set': {'ip_addr': ip_addr}})
 
-ip_addr = IpSpider.start_crawl('113.222.185.197')
-print(ip_addr)
+# ip_addr = IpSpider.start_crawl('113.222.185.197')
+# print(ip_addr)
