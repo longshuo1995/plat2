d5295b12fac0a86c698cc2d3563a81bbb2dff604
longshuo <longshuo@zuoyebang.com>
2019-05-22 20:30:31 +0800

    t

--- use_test/07_calc_order.py
+++ use_test/07_calc_order.py
@@ -9,3 +9,4 @@ while start_tm < tm:
     time_interval = 24*60*60
     update_order.start_update_order(time_interval, start_tm, current_time=start_tm+time_interval, is_correct=True)
     start_tm += time_interval
+    break
