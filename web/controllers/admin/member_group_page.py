d6585603ba35c3a4297887fb6527ada4fc8d1104
longshuo <longshuo@zuoyebang.com>
2019-05-27 17:12:15 +0800

    t

--- web/controllers/admin/member_group_page.py
+++ web/controllers/admin/member_group_page.py
@@ -19,8 +19,6 @@ def admin_login():
         req = request.form
         username = req.get('username')
         password = req.get('password')
-        logging.log(logging.ERROR, username)
-        logging.log(logging.ERROR, password)
         if username == 'qiyupingtuan' and password == 'd665e0369613cdcaddd4d268b3bcfb90':
             resp = make_response(redirect('/admin/list'))
             resp.set_cookie('username', username)
