546913ea0af13808356ed3bda2a3904b9b7702b9
longshuo <longshuo@zuoyebang.com>
2019-05-27 17:02:24 +0800

    t

--- web/controllers/admin/order_page.py
+++ web/controllers/admin/order_page.py
@@ -34,12 +34,31 @@ def admin_order():
     od = db_mongo.get_table('plat2', 'order').find(query)
     count = od.count()
     order_list = list(od.skip(pages*order_count_per_page).limit(order_count_per_page))
+
+    id_nick_map = {}
+
+    def query_nick_name(open_id):
+        if not open_id:
+            return ''
+        nick_name = id_nick_map.get(open_id, '')
+        if not nick_name:
+            info = db_mongo.get_table('plat2', 'member').find_one({'open_id': open_id})
+            if not info:
+                info = {}
+            id_nick_map[open_id] = info.get('nick_name')
+        return id_nick_map[open_id]
+    for item in order_list:
+        item['nick_open_id'] = query_nick_name(item['custom_parameters'])
+        item['nick_refer_id'] = query_nick_name(item['refer_id'])
+        item['nick_leader_openid'] = query_nick_name(item['leader_openid'])
+        item['nick_leader_master'] = query_nick_name(item['leader_master'])
+
     max_count = math.ceil(count/10)-1
     before_page = pages - 1
-    before_page = 0 if before_page < 0 else 1
+    before_page = 0 if before_page < 0 else before_page
 
     after_page = pages + 1
-    after_page = max_count if after_page < max_count else max_count
+    after_page = after_page if after_page < max_count else max_count
     base_url = '/admin/order?open_id=%s&mix_kw=%s' % (open_id, mix_kw)
     data['before_page'] = base_url + '&pages=%s' % before_page
     data['after_page'] = base_url + '&pages=%s' % after_page
